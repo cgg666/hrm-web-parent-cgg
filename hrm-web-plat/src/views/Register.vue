b0bb2d735e688f3bfa6813abec8610b64b350f87
cgg666 <1002022813@qq.com>
2020-06-20 09:20:10 +0800

    修改前端

--- hrm-web-plat/src/views/Register.vue
+++ hrm-web-plat/src/views/Register.vue
@@ -1,74 +1,74 @@
 <template>
-    <el-form :model="tenant" :rules="tenantRules" ref="tenantForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-        <h3 class="title">公司入驻</h3>
-        <el-form-item label="公司名称" label-width="80px" prop="companyName">
-            <el-input type="text" v-model="tenant.companyName" auto-complete="off" placeholder="请输入公司名称"></el-input>
-        </el-form-item>
-        <el-form-item  label="公司电话" label-width="80px" prop="companyNum">
-            <el-input type="text" v-model="tenant.companyNum" auto-complete="off" placeholder="请输入公司电话"></el-input>
-        </el-form-item>
-        <el-form-item  label="机构类型" label-width="80px">
-            <!--<el-input type="text" v-model="tenant.tenantType" auto-complete="off" placeholder="请选择机构类型"></el-input>-->
-            <el-select v-model="tenant.tenantType" clearable placeholder="请选择机构类型" style="width: 100%">
-                <el-option
-                        v-for="item in tenantTypes"
-                        :key="item.id"
-                        :label="item.name"
-                        :value="item.id">
-                </el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item  label="套餐选择" label-width="80px">
-            <el-select v-model="tenant.mealId" clearable placeholder="请选择套餐" style="width: 100%">
-                <el-option
-                        v-for="item in meals"
-                        :key="item.id"
-                        :label="item.mealName"
-                        :value="item.id">
-                    <span style="float: left">{{ item.mealName }}</span>
-                    <span style="float: right; color: #8492a6; font-size: 13px">{{ item.mealPrice }}</span>
-                </el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item  label="公司地址" label-width="80px">
-            <el-input type="text" v-model="tenant.address" auto-complete="off" placeholder="请输入公司地址"></el-input>
-        </el-form-item>
-        <el-form-item  label="账号" label-width="80px" prop="username">
-            <el-input type="text" v-model="tenant.username" auto-complete="off" placeholder="请输入公司账号"></el-input>
-        </el-form-item>
-        <el-form-item  label="密码" label-width="80px" prop="password">
-            <el-input type="password" v-model="tenant.password" auto-complete="off" placeholder="请输入公司密码"></el-input>
-        </el-form-item>
-        <el-form-item  label="确认密码" label-width="80px" prop="confirmPassword">
-            <el-input type="password" v-model="tenant.confirmPassword" auto-complete="off" placeholder="请再次输入密码"></el-input>
-        </el-form-item>
-        <el-form-item  label="公司Logo" label-width="80px">
-            <!--<el-input type="text" v-model="tenant.logo" auto-complete="off" placeholder="请上传公司Logo"></el-input>-->
-            <!--
-              action : 文件上传接口
-              file-list：回显
-              list-type 文件缩略图
-              limit 限制上传的文件个数 老版本不支持
-            -->
-            <el-upload
-                    action="http://localhost:1299/services/file/upload"
-                    :on-success="handleLogoSuccess"
-                    :limit="1"
-                    list-type="picture">
-                <el-button size="small" type="primary">点击上传</el-button>
-            </el-upload>
+  <el-form :model="tenant" :rules="tenantRules" ref="tenantForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">公司入驻</h3>
+    <el-form-item label="公司名称" label-width="80px" prop="companyName">
+      <el-input type="text" v-model="tenant.companyName" auto-complete="off" placeholder="请输入公司名称"></el-input>
+    </el-form-item>
+    <el-form-item  label="公司电话" label-width="80px" prop="companyNum">
+      <el-input type="text" v-model="tenant.companyNum" auto-complete="off" placeholder="请输入公司电话"></el-input>
+    </el-form-item>
+    <el-form-item  label="机构类型" label-width="80px">
+      <!--<el-input type="text" v-model="tenant.tenantType" auto-complete="off" placeholder="请选择机构类型"></el-input>-->
+      <el-select v-model="tenant.tenantType" clearable placeholder="请选择机构类型" style="width: 100%">
+        <el-option
+                v-for="item in tenantTypes"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+        </el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item  label="套餐选择" label-width="80px">
+      <el-select v-model="tenant.mealId" clearable placeholder="请选择套餐" style="width: 100%">
+        <el-option
+                v-for="item in meals"
+                :key="item.id"
+                :label="item.mealName"
+                :value="item.id">
+          <span style="float: left">{{ item.mealName }}</span>
+          <span style="float: right; color: #8492a6; font-size: 13px">{{ item.mealPrice }}</span>
+        </el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item  label="公司地址" label-width="80px">
+      <el-input type="text" v-model="tenant.address" auto-complete="off" placeholder="请输入公司地址"></el-input>
+    </el-form-item>
+    <el-form-item  label="账号" label-width="80px" prop="username">
+      <el-input type="text" v-model="tenant.username" auto-complete="off" placeholder="请输入公司账号"></el-input>
+    </el-form-item>
+    <el-form-item  label="密码" label-width="80px" prop="password">
+      <el-input type="password" v-model="tenant.password" auto-complete="off" placeholder="请输入公司密码"></el-input>
+    </el-form-item>
+    <el-form-item  label="确认密码" label-width="80px" prop="confirmPassword">
+      <el-input type="password" v-model="tenant.confirmPassword" auto-complete="off" placeholder="请再次输入密码"></el-input>
+    </el-form-item>
+    <el-form-item  label="公司Logo" label-width="80px">
+      <!--<el-input type="text" v-model="tenant.logo" auto-complete="off" placeholder="请上传公司Logo"></el-input>-->
+      <!--
+        action : 文件上传接口
+        file-list：回显
+        list-type 文件缩略图
+        limit 限制上传的文件个数 老版本不支持
+      -->
+      <el-upload
+              action="http://localhost:1299/services/file/upload"
+              :on-success="handleLogoSuccess"
+              :limit="1"
+              list-type="picture">
+        <el-button size="small" type="primary">点击上传</el-button>
+      </el-upload>
 
-        </el-form-item>
+    </el-form-item>
 
-        <el-form-item style="width:100%;">
-            <el-col :span="11">
-                <el-button type="primary" style="width:100%;" @click.native.prevent="handlerRegister" :loading="regLogining">注册</el-button>
-            </el-col>
-            <el-col :span="11" :offset="2">
-                <el-button type="primary" style="width:100%;" @click.native.prevent="handlerLogin" >已有账号，登录</el-button>
-            </el-col>
-        </el-form-item>
-    </el-form>
+    <el-form-item style="width:100%;">
+      <el-col :span="11">
+        <el-button type="primary" style="width:100%;" @click.native.prevent="handlerRegister" :loading="regLogining">注册</el-button>
+      </el-col>
+      <el-col :span="11" :offset="2">
+        <el-button type="primary" style="width:100%;" @click.native.prevent="handlerLogin" >已有账号，登录</el-button>
+      </el-col>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
@@ -192,25 +192,25 @@
 </script>
 
 <style lang="scss" scoped>
-    .login-container {
-        /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
-        -webkit-border-radius: 5px;
-        border-radius: 5px;
-        -moz-border-radius: 5px;
-        background-clip: padding-box;
-        margin: 50px auto;
-        width: 500px;
-        padding: 35px 35px 15px 35px;
-        background: #fff;
-        border: 1px solid #eaeaea;
-        box-shadow: 0 0 25px #cac6c6;
-        .title {
-            margin: 0px auto 40px auto;
-            text-align: center;
-            color: #505458;
-        }
-        .remember {
-            margin: 0px 0px 35px 0px;
-        }
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 50px auto;
+    width: 500px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
     }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+  }
 </style>
